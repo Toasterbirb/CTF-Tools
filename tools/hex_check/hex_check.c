0170e8dc5a238970bfe965a69d820fed6f907b54
Toasterbirb <git@toasterbirb.com>
2023-11-25 21:33:55 +0200

    Add stdin support for longer strings

--- tools/hex_check/hex_check.c
+++ tools/hex_check/hex_check.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 #include <string.h>
 
 int is_hex_char(char c)
@@ -15,31 +16,63 @@ void print_not_hex(char* hex_value)
 
 int main(int argc, char** argv)
 {
-	if (argc != 2)
+	if (argc > 2)
 	{
-		printf("Usage: %s [suspected hex string]\n", argv[0]);
+		printf("Usage: hex_check [suspected hex string]\n");
+		printf("\nYou can also pipe text to stdin\n");
 		return 1;
 	}
 
-	int len = strlen(argv[1]);
-
-	/* Hex values are always divisible by two (assuming they come in pairs of two) */
-	if (len % 2 != 0)
+	/* Reading from stdin */
+	if (argc == 1)
 	{
-		print_not_hex(argv[1]);
-		return 1;
+		char c;
+		int char_count = 0;
+		while (read(STDIN_FILENO, &c, 1) > 0)
+		{
+			char_count++;
+			if (!is_hex_char(c) && c != '\0' && c != '\n')
+			{
+				printf("The provided string was not a hex string\n");
+				return 1;
+			}
+		}
+
+		/* Hex values are always divisible by two (assuming they come in pairs of two) */
+		if ((char_count - 1) % 2 != 0)
+		{
+			printf("The provided string was not a hex string\n");
+			return 1;
+		}
+
+		printf("The provided string is a hex string\n");
+		return 0;
 	}
 
-
-	for (int i = 0; i < len; ++i)
+	/* Reading from an argument */
+	if (argc == 2)
 	{
-		if (!is_hex_char(argv[1][i]))
+		int len = strlen(argv[1]);
+
+		/* Hex values are always divisible by two (assuming they come in pairs of two) */
+		if (len % 2 != 0)
 		{
 			print_not_hex(argv[1]);
 			return 1;
 		}
+
+
+		for (int i = 0; i < len; ++i)
+		{
+			if (!is_hex_char(argv[1][i]))
+			{
+				print_not_hex(argv[1]);
+				return 1;
+			}
+		}
+
+		printf("%s is a hex string\n", argv[1]);
 	}
 
-	printf("%s is a hex string\n", argv[1]);
 	return 0;
 }
